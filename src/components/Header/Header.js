ca428c58d20b2217b7a160ca1b559a27c2e1b109
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-09-29 10:30:25 +0600

    resources and folders created with fack data also

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Sports Across The Globe</h1>
+      <p>Choose your sports</p>
+    </div>
+  );
+};
+
+export default Header;
